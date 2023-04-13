00fae018cfc2bfe0b83710d2b6877d26095cefa7
User <7b4s8zl@code.edu.az>
2023-04-13 20:01:37 +0400

    added file

---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+// var eded = 29;
+
+// var teklik = eded % 10;
+// eded =(eded - teklik)/10;
+// var eks = teklik*10+eded;
+// console.log(eks);
+
+
+
+//1den 100e qeder cut ededlerin cemini tapin              while ile yazilisi
+
+// var i=1;
+// while(i<=100 ){
+// if(i%2==0){
+//     sum+=i
+// }
+// i++
+
+// }
+// alert(sum)
+
+
+
+
+// //do while ile yazilisi
+
+// var i=1;
+// {
+// if(i%2==0){
+//     sum+=i
+// }
+// i++
+
+// }
+// while(i<=100 )
+
+// alert(sum)
+
+
+
+
+
+
+// //for ile yazilisi
+
+// var sum = 0;
+// for(var i =1; i<=100 i++){
+//     if(i%2==0){
+//         sum +=i;
+
+//     }
+// }
+// alert(sum)
+
+
+
+
+// 2. 1den 100 e qeder tek ededlerin hasilini tapin
+
+//for ile yazilisi
+//  var sum = 1;
+//  var i=1;
+//  for(var i=1; i<=100; i++){
+//     if(i%2==1){
+//         sum *=i;
+//  }
+//  }
+//  alert(sum)
+
+
+
+
+ //while ile
+
+//  var i=1;
+// var sum=1;
+// while(i<=100){
+//    if(i%2==1){
+//        sum *= i
+//    }
+//    i++
+// }
+// alert(sum)
+
+
+
+
+
+
+//do while
+// var i=1;
+// var sum=1;
+// do{
+//    if(i%2==1){
+//        sum *= i
+//    }
+//    i++
+// }
+// while(i<=10)
+// alert(sum)
+
+
+
+
+
+//3. 1den 100e qeder 15e bolunen ededlerin cemini tapin      For ile yazilisi
+// var sum =0;
+// var count =0;    //count 100un icinde nece dene 15e eded oldugun sayini gosterir
+//burda eger var numpromote("") yazilarsa bizim sertde 100 yazmagimiza ehtiyac yoxdu num yazacaq ve istifadeci promote hissesine ne reqem yazsa onu hesablayacaq
+// for(var i=1; i<=100; i++){
+//     if(i%15==0){
+//         sum+=i;
+//         count++;
+//     }
+// }
+// var avg=sum/count;     //avg ededi ortadi      bunu yuxarida count un altinda da vermek olar
+// alert(avg)
+
+
+
+
+
+//while ile yazilisi
+
+
+
+
